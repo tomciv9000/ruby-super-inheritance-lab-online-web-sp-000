9a32622a7a8d90da10f02c76807db33e194f1d39
Tom White <eugene.white@gmail.com>
2019-06-15 04:55:06 +0000

    Automatically backed up by Learn

---
+++ lib/student.rb
@@ -0,0 +1,14 @@
+require "./chatty_student.rb"
+
+class Student 
+  
+  def hello 
+    puts "Hey there! I'm so exited to learn stuff."
+  end
+  
+  def raise_hand
+    puts "Pick me!"
+  end
+  
+  
+end
